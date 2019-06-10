b9f40b8c8b1c30900a635f6f75b1cc8d23b3101c
rechie <502117806@qq.com>
2019-06-10 17:27:47 +0800

    test: 添加配置文件的测试

---
+++ test/test_zbspider.py
@@ -0,0 +1,11 @@
+"""
+对 zbspider 进行测试
+"""
+import unittest
+from zbspider import *
+
+class TestZBSpider(unittest.TestCase):
+    """ test class zbspider """
+
+    def test_config():
+        pass
