40bbcbec350feb88451a8ba753e6905b462dd38f
rechie <502117806@qq.com>
2019-07-01 17:54:37 +0800

    Test: 添加测试用例
    1、添加测试配置
    2、添加配置测试
    3、添加爬取测试
    4、添加数据表名测试
    5、添加测试数据表最后一个数据

--- test/test_zbspider.py
+++ test/test_zbspider.py
@@ -2,10 +2,36 @@
 对 zbspider 进行测试
 """
 import unittest
-from zbspider import *
+from config import TestConfig
+from zbspider.spider import Spider
+from zbspider.mode.sqlmanager import SQLManager
+
 
 class TestZBSpider(unittest.TestCase):
     """ test class zbspider """
 
-    def test_config():
-        pass
+    def setUp(self):
+        self.verifyString = 'test'
+        self.config = TestConfig()
+
+    def test_config(self):
+        self.assertTrue(self.config.TESTING)
+        self.assertTrue(self.config.DEBUG)
+
+    def test_spider_get(self):
+        spider = Spider(self.config.URL, **self.config.SPIDER)
+        result = spider.get()
+        self.assertIsInstance(result, str)
+        self.assertIn('中国招标与采购网', result)
+        self.assertIn('福建', result)
+
+    def test_table_name(self):
+        sql = SQLManager(self.config.DATABASE)
+        self.assertEqual("zbtable", sql.__tablename__)
+        sql.close()
+
+    def test_last_one_title(self):
+        sql = SQLManager(self.config.DATABASE)
+        last_one_title = sql.get_last_one_title()
+        self.assertIsInstance(last_one_title, dict)
+        sql.close()
